e64cea95b36fdca0f7426630744a18f1bfb05f0b
Gustavo Marty Sroka <gustavo.marty@hotmail.com>
2018-01-09 18:47:53 -0200

    m

--- js/javascript.js
+++ js/javascript.js
@@ -1,21 +1,39 @@
+var data = [
+    {
+        "id": 1,
+        "nome": "Marty",
+        "email": "email@teste.com",
+        "telefone": "41 999999999"
+    },
+    {
+        "id": 2,
+        "nome": "Joao",
+        "email": "joao@teste.com",
+        "telefone": "41 888888888"
+    },
+    {
+        "id": 3,
+        "nome": "Maria",
+        "email": "maria@teste.com",
+        "telefone": "41 988889999"
+    }
+];
+
+
 $(document).ready(function(){
     listarPessoas();
 })
 
-var data = [
-    {
-        "id": 1,
-        "nome": "Marty"
-    },
-    {
-        "id": 2,
-        "nome": "Joao"
-    },
-    {
-        "id": 3,
-        "nome": "Maria"
-    }
-];
+function cadastrar()
+{
+    var nome = $('#txtNome').val();
+    
+    $('#resultado').html('o nome e ' + nome);
+
+    //var nome = $('#iddomeucamponome').
+
+}
+
 
 
 function listarPessoas()
@@ -25,6 +43,8 @@ function listarPessoas()
         var cols = "";		
         cols += '<td>' + item.id + '</td>';		    
         cols += '<td>' + item.nome + '</td>';		    
+        cols += '<td>' + item.email + '</td>';	
+        cols += '<td>' + item.telefone + '</td>';	
         cols += '<td>';		    
         cols += '<button onclick="RemoveTableRow(this)" type="button">Remover</button>';		    
         cols += '</td>';		
